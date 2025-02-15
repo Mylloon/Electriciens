bbcb505750f86115949b64b0b82919f0478807aa
2-1-1-2 <tanyan.francois@gmail.com>
2025-02-15 17:17:44 +0100

    Filter every 3s

--- Scripts/FilterColor.gd
+++ Scripts/FilterColor.gd
@@ -1,12 +1,11 @@
-class_name FilterColor
+class_name FilterColor 
+
 
 static var current_color: ColorsEnum.Colors = ColorsEnum.Colors.RED
-
 
 static func next() -> Color:
 	current_color = ColorsEnum.Colors.values()[(current_color + 1) % ColorsEnum.colors.size()]
 	return current()
-
 
 static func current(name: bool = false) -> Variant:
 	if name:
