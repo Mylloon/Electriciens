511affd01e2542c842c7e3d570e57e3937437aa2
Mylloon <kennel.anri@tutanota.com>
2025-02-16 05:08:28 +0100

    add volume setting

--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -2,6 +2,18 @@ extends Control
 
 @onready var switcher = get_node("/root/PauseHistory")
 
+@onready var volume: HSlider = $HBoxContainer/Volume
+@export var audio_bus_name := "Master"
+@onready var bus := AudioServer.get_bus_index(audio_bus_name)
+
+
+func _ready() -> void:
+	volume.value = db_to_linear(AudioServer.get_bus_volume_db(bus))
+
 
 func _on_button_pressed() -> void:
 	switcher.return_to_last()
+
+
+func _on_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(bus, linear_to_db(value))
