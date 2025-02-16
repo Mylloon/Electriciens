a43d62d0a5bcf577caf4e15769c19d847badd56c
2-1-1-2 <tanyan.francois@gmail.com>
2025-02-16 15:28:38 +0100

    Spawner + Principal things implemented

---
+++ Scripts/Player_spawner.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var player_scene = preload("res://Characters/Player.tscn")
+var player = null
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player == null:
+		var new_obj = player_scene.instantiate()
+		var posX : int = position.x
+		var posY : int = position.y
+		new_obj.position.x =  posX
+		new_obj.position.y =  posY
+		get_parent().add_child(new_obj)
+		player = new_obj
--- Scripts/Success.gd
+++ Scripts/Success.gd
@@ -3,14 +3,28 @@ extends Node2D
 @onready var switcher = get_node("/root/PauseHistory")
 
 @onready var success: TileMapLayer = $TileMap/floor/success
+@onready var walls: TileMapLayer = $TileMap/walls
 @onready var player: Player = $Player
 
 
 func _physics_process(_delta: float) -> void:
-	var tile_loc := success.local_to_map(player.position)
-	var tile := success.get_cell_tile_data(tile_loc)
-	if tile and tile.get_custom_data("coin"):
-		switcher.switch_scene("res://Scenes/LevelChooser.tscn")
+	if(!player):
+		player = $Player
+		return
+	else :	
+		var tile_loc := success.local_to_map(player.position)
+		var tile := success.get_cell_tile_data(tile_loc)
+		if tile and tile.get_custom_data("coin"):
+			switcher.switch_scene("res://Scenes/LevelChooser.tscn")
+			return
+		
+		for el:TileMapLayer in walls.get_children():
+			var tile_loc_laser := el.local_to_map(player.position)
+			var tile_laser := el.get_cell_tile_data(tile_loc_laser)
+			if tile_laser and tile_laser.get_custom_data("laser") and el.enabled:
+				player.queue_free()
+		
+		
 
 
 func _process(_delta: float) -> void:
